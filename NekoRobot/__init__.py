b89692b4df9a44c1a3c9c6ff5481808f887d27f2
Shinigamiiz <119001740+Shinigamiiz@users.noreply.github.com>
2022-12-04 21:14:25 +0530

    Update __init__.py

--- NekoRobot/__init__.py
+++ NekoRobot/__init__.py
@@ -32,9 +32,15 @@ import time
 import aiohttp
 import httpx
 import spamwatch
+import pymongo
 import telegram.ext as tg
+from redis import StrictRedis
 from aiohttp import ClientSession
 from httpx import AsyncClient, Timeout
+from pymongo import MongoClient 
+from odmantic import AIOEngine
+from NekoRobot.confing import get_int_key, get_str_key
+from motor import motor_asyncio
 from pyrogram import Client
 from pyrogram.enums import ParseMode
 from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, PeerIdInvalid
@@ -115,6 +121,7 @@ if ENV:
     API_HASH = os.environ.get("API_HASH", None)
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
     DB_URI = os.environ.get("DATABASE_URL")
+    DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     DONATION_LINK = os.environ.get("DONATION_LINK")
     LOAD = os.environ.get("LOAD", "").split()
@@ -139,19 +146,20 @@ if ENV:
     IBM_WATSON_CRED_PASSWORD = os.environ.get("IBM_WATSON_CRED_PASSWORD", None)
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
-    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
     TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "lightYagami")
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
     BOT_NAME = os.environ.get("BOT_NAME", True)  # Name Of your Bot.4
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")  # Bot Username
     HELP_IMG = os.environ.get("HELP_IMG", True)
+    REDIS_URL = os.environ.get("REDIS_URL", None) # REDIS URL (From:- Heraku & Redis)
     OPENWEATHERMAP_ID = os.environ.get(
         "OPENWEATHERMAP_ID", ""
     )  # From:- https://openweathermap.org/api
     LOG_GROUP_ID = os.environ.get("LOG_GROUP_ID", None)
-    BOT_ID = 1412878118
+    BOT_ID = 5722771565
     STRICT_GMUTE = bool(os.environ.get("STRICT_GMUTE", True))
+    MONGO_DB = "Shikimori"
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
     REM_BG_API_KEY = os.environ.get(
         "REM_BG_API_KEY", None
@@ -207,8 +215,12 @@ else:
     REDIS_URL = Config.REDIS_URL
     DONATION_LINK = Config.DONATION_LINK
     LOAD = Config.LOAD
+    HELP_IMG = Config.HELP_IMG
     NO_LOAD = Config.NO_LOAD
+    ERROR_LOGS = Config.ERROR_LOGS
     DEL_CMDS = Config.DEL_CMDS
+    MONGO_DB = Config.MONGO_DB
+    MONGO_DB_URI = Config.MONGO_DB_URI
     STRICT_GBAN = Config.STRICT_GBAN
     WORKERS = Config.WORKERS
     BAN_STICKER = Config.BAN_STICKER
@@ -221,11 +233,13 @@ else:
     SPAMWATCH_SUPPORT_CHAT = Config.SPAMWATCH_SUPPORT_CHAT
     SPAMWATCH_API = Config.SPAMWATCH_API
     INFOPIC = Config.INFOPIC
-    ARQ_API_URL = Config.ARQ_API_URL
-    ARQ_API_KEY = Config.ARQ_API_KEY
+    TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
+    BOT_NAME = Config.BOT_NAME
+    ARQ_API_URL = "arq.hamker.dev"
+    ARQ_API_KEY = "ZDXDYF-ISSFYB-RGVJWR-DFTDWI-ARQ"
 
-    BOT_USERNAME = Config.BOT_USERNAME
-    OPENWEATHERMAP_ID = Config.OPENWEATHERMAP_ID
+    BOT_USERNAME = "AnyaXForgerbot"
+    OPENWEATHERMAP_ID = "ca1f9caacbb92187db96c0bf5686017b"
 
     REM_BG_API_KEY = Config.REM_BG_API_KEY
 
@@ -236,6 +250,20 @@ else:
 
 
 DEV_USERS.add(1768190240)
+DEV_USERS.add(5667156680)
+REDIS = StrictRedis.from_url(REDIS_URL, decode_responses=True)
+REDIS_URL = "redis://default:6vsajwd6wq3lNI7T3zMIpltTrU040DJC@redis-19248.c16.us-east-1-3.ec2.cloud.redislabs.com:19248"
+
+try:
+    REDIS.ping()
+    LOGGER.info("Your redis server is now alive!")
+
+except BaseException:
+    raise Exception("Your redis server is not alive, please check again.")
+
+finally:
+   REDIS.ping()
+   LOGGER.info("Your redis server is now alive!")
 
 if not SPAMWATCH_API:
     sw = None
@@ -262,6 +290,13 @@ print(
 print(
     "[NEKOROBOT] Project Maintained By: github.com/Awesome-Prince (https://github.com/Awesome-Prince/NekoRobot-3)"
 )
+
+
+STRICT_GMUTE = "yes"
+mongodb = MongoClient(MONGO_DB_URI, 27017)[MONGO_DB]
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI)
+db = motor[MONGO_DB]
+engine = AIOEngine(motor, MONGO_DB)
 
 print("[NEKOROBOT]: Telegraph Installing")
 telegraph = Telegraph()
